1bcb136e25b59f871d4bcda24b63ae5958f80220
lilo <l.long@cern.ch>
2024-04-21 18:24:10 +0200

    add configure files for compiling

---
+++ ePIC_SVT_OB_Detector/source/autogen.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#=======================================================================
+#   Copyright (C) 2024 Univ. of Bham  All rights reserved.
+#   
+#   		FileName：		autogen.sh
+#   	 	Author：		LongLI <long.l@cern.ch>
+#   		Time：			2024.04.21
+#   		Description：
+#
+#======================================================================
+
+srcdir = `dirname $0`
+test -z "$srcdir" && srcdir=.
+
+(cd $srcdir; aclocal -I ${OFFLINE_MAIN}/share;\
+ libtoolize --force; automake -a --add-missing; autoconf)
+
+$srcdir/configure "$@"
