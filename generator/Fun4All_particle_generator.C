a1bd1d786d1db9f2467c9b5f07366725f898dc1c
lilo <l.long@cern.ch>
2024-05-14 10:10:23 +0100

    add particle generator module

---
+++ generator/Fun4All_particle_generator.C
@@ -0,0 +1,19 @@
+/*========================================================================
+*   Copyright (C) 2024 Univ. of Bham. All rights reserved.
+*   
+*   	FileName：		Fun4All_particle_generator.C
+*   	Author：		LongLI <long.l@cern.ch>
+*   	Time：			2024.05.14
+*   	Description：	
+*
+=========================================================================*/
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "TFile.h"
+#include "TList.h"
+#include "TH1D.h"
+#include "TGraph.h"
+
+using namespace std;
